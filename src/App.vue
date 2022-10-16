19d5be30046ea6f249c83e7bc47802381411e620
a3521110287@alumno.uttehuacan.edu.mx <a3521110287@alumno.uttehuacan.edu.mx>
2022-10-16 00:16:54 -0500

    first value

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+  <template>
+  <div class="container mt-3">
+    <div class="row">
+      <div class='col-12 col-md-4'>
+    <h1> App- Proyectos </h1>
+    </div>
+    <img width="100" :src="url" alt="des">
+      </div>
+    </div>
+    <hr/>
+
+
+
+   <form @submit.prevent="addUser">
+    <div>
+    <label>
+      Nombre:
+    <input v-model="nombre" type="text" placeholder="Nombre"  required /> </label></div>
+ 
+  <div><label>
+      Edad:
+    <input  v-model="edad"  type="number" min="1" max="80" placeholder="Edad" required /> </label>
+  </div>
+  <div>
+    <label>
+      Correo:
+    <input  v-model="correo"  type="email" placeholder="email@mail.com"  required /> </label>
+  </div>
+
+    <button type="submit">Enviar</button>
+
+</form>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    url: "https://cdn.svgporn.com/logos/vue.svg",
+    des: "Vue.js",
+    nombre: "",
+    edad: 0,
+    correo:"",
+  }),
+  methods: {
+    adduser() {
+      console.log(this.nombre, this.edad, this.correo);
+    },
+  },
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,85 @@
+<script>
+    export default {
+        data:() => ({
+            proyecto:"",
+            tipo: "",
+            urgente:"",
+            proyectos:[], 
+        }),
+        methods: {
+            registrarProyecto () {
+                const proyecto ={
+                    proyecto : this.proyecto,
+                    tipo : this.tipo,
+                    urgente.urgente= false;
+                };
+                this.proyectos.push(proyecto);
+
+                console.log(proyecto)
+        },
+    },
+};
+
+</script>
+
+
+
+
+
+
+
+<template>
+<form @submit.prevent="registrarProyecto">
+
+<div class="mb-3">
+    <label class="form-label"> Proyecto</label>
+    <input v-model.trim ="urgente" type="text" class="form-control"/>    //Trim: Limpiar espacios //
+</div>
+<div class="mb-3">
+    <label class="form-label">Actividad</label>
+<select class="form select" required>
+    <option disabled selected value=""> Seleccione el tipo de actividad ...</option>
+    <option value="">  ...</option>
+    <option value=""> Backend service con Node.Js...</option>
+    <option value=""> App movil con React Native ...</option>
+
+</select>
+</div>
+
+<div class="mb-3">
+    <label for="exampleInputPassword" class="form-check-label"> Urgente </label>
+    <input type="checkbox" class="form-check-input">
+</div>
+
+    <button type="submit" class="btn btn-primary"> Guardar</button>
+
+</form>
+
+<hr />
+<div class="table.responsive">
+    <table class="table">
+        <thead>
+            <tr>
+                <th> # </th>
+                <th>Proyecto</th>
+                <th>Tipo</th>
+                <th>Urgente</th>
+
+            </tr>
+        </thead>
+                
+        <tbody>
+            <tr v-for="(proyecto, index) in proyectos" :key="index">
+               <td>{{Index + 1}}</td>
+               <td>{{proyecto.proyecto}}</td>
+               <td>{{proyecto.tipo}}</td>
+               <td class="proyecto.urgente ? 'bg-success' : 'bg-danger' ">{{proyecto.urgente ? "Si" : "No" }}</td>
+
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+
+
+*//Pintar una variable con corchetes//*
